06dfbeb20df2ae7d4cb5d2337702b2f494f48270
Dariofil Civonitram <dariofil@hotmail.com>
2024-09-21 09:29:11 +1000

    Add project files.

---
+++ ConsoleTextEffects/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleTextEffects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("These examples will run Synchronously...");
+
+            TextEffect.MatrixText(1, "This is an example of matrix text effect This is an example of matrix text effect").Wait();
+
+            TextEffect.SlideText(2, "This is an example of slide text effect", 20).Wait();
+
+            TextEffect.TrickleText(3, "This is an example of trickle text effect", 1, Console.BufferWidth / 3).Wait();
+
+            TextEffect.CenterText(4, "Example of centered text", Console.BufferWidth).Wait();
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            TextEffect.WriteBlinkingText(5, "Example of blinking text", 150, cancellationTokenSource).Wait(TimeSpan.FromSeconds(10));
+            cancellationTokenSource.Cancel();
+
+            Console.WriteLine("Press any key to try Asynchronously...");
+            Console.ReadKey();
+
+
+            Console.Clear();
+            Console.WriteLine("These examples will run Asynchronously...");
+            //You will notice blinking text slows things down significantly because of thread delay and lockobject...
+
+            TextEffect.MatrixText(1, "This is an example of matrix text effect This is an example of matrix text effect");
+
+            TextEffect.SlideText(2, "This is an example of slide text effect", 50, Console.BufferWidth);
+
+            TextEffect.TrickleText(3, "This is an example of trickle text effect", 5, Console.BufferWidth / 2);
+
+            TextEffect.CenterText(4, "Example of centered text", Console.BufferWidth);
+
+            var cancellationTokenSource2 = new CancellationTokenSource();
+            TextEffect.WriteBlinkingText(5, "Example of blinking text", 150, cancellationTokenSource2).Wait(TimeSpan.FromSeconds(10));
+            cancellationTokenSource2.Cancel();
+
+            Console.WriteLine("Done! Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleTextEffects/TextEffect.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleTextEffects
+{
+    public static class TextEffect
+    {
+        private static object _lockObject = new object();
+        public static Task SlideText(int line, string text, int delay = 100, int width = 0)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                if (width == 0)
+                    width = Console.BufferWidth - text.Length;
+
+                var fullString = string.Empty.PadRight(width) + text;
+
+                for (var i = width; i > 0; i--)
+                {
+                    fullString = fullString.Remove(0, 1);
+                    fullString = fullString + " ";
+
+                    lock (_lockObject)
+                    {
+                        Console.CursorTop = line;
+                        Console.CursorLeft = 0;
+                        Console.Write(fullString);
+                    }
+
+                    Thread.Sleep(delay);
+                }
+            });
+        }
+
+        public static Task TrickleText(int line, string text, int delay = 100, int length = 0)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                if (length == 0)
+                    length = Console.BufferWidth - text.Length;
+
+                var fullString = string.Empty.PadRight(length) + text;
+
+                for (var x = 0; x < text.Length; x++)
+                {
+                    for (var i = length; i > 0; i--)
+                    {
+                        fullString = fullString.Remove(x, 1);
+                        fullString = fullString.Insert(length + x, " ");
+
+                        lock (_lockObject)
+                        {
+                            Console.CursorLeft = 0;
+                            Console.CursorTop = line;
+                            Console.Write(fullString);
+                        }
+
+                        Thread.Sleep(delay);
+                    }
+                }
+            });
+        }
+
+        public static Task MatrixText(int line, string text, int delay = 5)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var random = new Random(Thread.CurrentThread.GetHashCode());
+                var chars =
+                    "#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+
+
+                var matrixString = string.Empty;
+
+                foreach (var c in text)
+                {
+                    if (c == 32)
+                    {
+                        matrixString = matrixString + " ";
+                        continue;
+                    }
+
+                    matrixString = matrixString + chars[random.Next(0, chars.Length)];
+                }
+
+                var scrambledString = matrixString;
+
+                while (!string.Equals(scrambledString, text))
+                {
+                    lock (_lockObject)
+                    {
+                        Console.CursorTop = line;
+                        Console.CursorLeft = 0;
+                        Console.Write(scrambledString);
+                    }
+
+                    var index = 0;
+                    foreach (var c in scrambledString)
+                    {
+                        if (c == 32 || c.ToString() == text.Substring(index, 1))
+                        {
+                            index++;
+                            continue;
+                        }
+
+                        scrambledString = scrambledString.Remove(index, 1)
+                            .Insert(index, chars[random.Next(0, chars.Length)].ToString());
+
+                        index++;
+                    }
+
+                    Thread.Sleep(delay);
+                }
+
+                lock (_lockObject)
+                {
+                    Console.CursorLeft = 0;
+                    Console.CursorTop = line;
+                    Console.WriteLine(scrambledString);
+                }
+            });
+        }
+
+        public static Task CenterText(int line, string text, int width = 80)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                lock (_lockObject)
+                {
+                    Console.CursorTop = line;
+                    Console.CursorLeft = (width / 2) - (text.Length / 2);
+                    Console.WriteLine(text);
+                }
+            });
+        }
+
+        public static Task WriteBlinkingText(int line, string text, int delay, CancellationTokenSource cancellationTokenSource)
+        {
+            if (cancellationTokenSource == null)
+                cancellationTokenSource = new CancellationTokenSource();
+
+            return Task.Factory.StartNew(() =>
+            {
+                bool visible = true;
+                while (!cancellationTokenSource.IsCancellationRequested)
+                {
+                    lock (_lockObject)
+                    {
+                        Console.CursorTop = line;
+                        Console.CursorLeft = 0;
+                        Console.WriteLine((visible ? text : new string(' ', text.Length)));
+                        Thread.Sleep(delay);
+                        visible = !visible;
+                    }
+                }
+            }, cancellationTokenSource.Token);
+        }
+    }
+}
